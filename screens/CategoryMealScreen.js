9a2d06736a94f44089adbe778c36bf2040ee8042
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-26 16:22:17 -0400

    Send route and params when meal is clicked

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -19,7 +19,14 @@ const CategoryMealScreen = props => {
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
         image={itemData.item.imageURL}
-        onSelectMeal={() => {}}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
       />
     );
   };
