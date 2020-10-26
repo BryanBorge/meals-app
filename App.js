746a8cf08af7cdca62104dd20f3cbd7929c2b35f
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-26 13:59:35 -0400

    Added MealsNavigator

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+import MealsNavigator from "./Navigation/MealsNavigator";
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -19,9 +20,5 @@ export default function App() {
     );
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <MealsNavigator />;
 }
