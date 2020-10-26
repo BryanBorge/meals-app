c06af77a81e15e6620817fcfb026902032f00c4e
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-26 13:05:44 -0400

    Initial commit

---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, Stylesheet } from "react-native";
+
+const FavoriteScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Favorite Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default FavoriteScreen;
---
+++ screens/FilterScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, Stylesheet } from "react-native";
+
+const FilterScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Filter Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default FilterScreen;
