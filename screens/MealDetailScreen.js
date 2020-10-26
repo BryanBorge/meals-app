cae2b8a7eb0cd6205a6098a4776b76ff52bae15d
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-26 17:00:51 -0400

    now uses custom header button

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { MEALS } from "../data/dummyData";
-
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam("mealId");
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
@@ -28,7 +29,20 @@ MealDetailScreen.navigationOptions = navData => {
   const mealId = navData.navigation.getParam("mealId");
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
-  return { headerTitle: selectedMeal.title };
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Favorite'
+          iconName='ios-star'
+          onPress={() => {
+            console.log("mark as favorite");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 const styles = StyleSheet.create({
   screen: {
