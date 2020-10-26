4d172ab97425cb9da5610605adb95d81857b1346
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-26 14:57:08 -0400

    Using props to get selected category

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,18 +1,39 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import { CATEGORIES } from "../data/dummyData";
 
 const CategoryMealScreen = props => {
+  const catId = props.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title='GO TO DETAILS'
         onPress={() => {
           props.navigation.navigate({ routeName: "MealDetail" });
         }}
       />
+      <Button
+        title='GO BACK'
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
     </View>
   );
+};
+
+CategoryMealScreen.navigationOptions = navData => {
+  const catId = navData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+  };
 };
 
 const styles = StyleSheet.create({
