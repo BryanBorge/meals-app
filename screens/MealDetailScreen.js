731cb2cb01217aa53c03b876df875b002faf3b94
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-26 16:22:28 -0400

    set header title

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import { MEALS } from "../data/dummyData";
 
 const MealDetailScreen = props => {
+  const mealId = props.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>The Meal Details Screen</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title='GO BACK TO CATEGORIES'
         onPress={() => {
@@ -21,6 +24,12 @@ const MealDetailScreen = props => {
   );
 };
 
+MealDetailScreen.navigationOptions = navData => {
+  const mealId = navData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+  return { headerTitle: selectedMeal.title };
+};
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
