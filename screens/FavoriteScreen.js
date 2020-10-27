1f44ee7723ea54cf8d6f13b8e0c0f4c02d8b8ac5
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-27 15:09:47 -0400

    Added fallback text

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import MealList from "../components/MealList";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 
 import { useSelector } from "react-redux";
 
 const FavoriteScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
@@ -29,4 +39,11 @@ FavoriteScreen.navigationOptions = navData => {
   };
 };
 
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 export default FavoriteScreen;
