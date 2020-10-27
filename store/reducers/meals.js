cd16a2b0886e15760474bc97ed543afda7cdbcf9
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-27 14:21:07 -0400

    created meal reducer

---
+++ store/reducers/meals.js
@@ -0,0 +1,12 @@
+import { MEALS } from "../../data/dummyData";
+
+const initialState = {
+  meals: MEALS,
+  filteredMeals: MEALS,
+  favoriteMeals: [],
+};
+const mealsReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default mealsReducer;
