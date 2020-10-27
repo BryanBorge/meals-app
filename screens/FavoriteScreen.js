9bb6c4cde55ac90ffc5edcfb8b9c51471713d584
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-27 14:34:29 -0400

    now uses useSelector to get state from store

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
 import MealList from "../components/MealList";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
 
-import { MEALS } from "../data/dummyData";
+import { useSelector } from "react-redux";
 
 const FavoriteScreen = props => {
-  const favMeals = MEALS.filter(meal => meal.id === "m1" || meal.id === "m2");
+  const favMeals = useSelector(state => state.meals.favoriteMeals);
+
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
