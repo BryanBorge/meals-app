bac3e300f79dfa2ec87e00873186d21e1ad71c52
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-27 09:42:14 -0400

    added platform check for style

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -5,7 +5,7 @@ import {
   Text,
   StyleSheet,
   Platform,
-  TouchableNativeFeedback,
+  ToucheableNativeFeedback,
 } from "react-native";
 import Colors from "../constants/Colors";
 const CategoryGridTile = props => {
@@ -36,7 +36,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden",
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -48,7 +52,6 @@ const styles = StyleSheet.create({
       height: 2,
     },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
